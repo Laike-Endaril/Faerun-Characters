da85a21339570d965ac45f1cd46ab03f35e0f134
Kyle Koder <silvertallon@gmail.com>
2020-03-14 15:28:59 -0700

    Tabs

--- src/main/java/com/fantasticsource/faeruncharacters/CharacterCreationGUI.java
+++ src/main/java/com/fantasticsource/faeruncharacters/CharacterCreationGUI.java
@@ -4,6 +4,8 @@ import com.fantasticsource.mctools.gui.GUILeftClickEvent;
 import com.fantasticsource.mctools.gui.GUIScreen;
 import com.fantasticsource.mctools.gui.element.GUIElement;
 import com.fantasticsource.mctools.gui.element.text.GUIText;
+import com.fantasticsource.mctools.gui.element.text.GUITextButton;
+import com.fantasticsource.mctools.gui.element.view.GUIAutocroppedView;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -15,9 +17,24 @@ public class CharacterCreationGUI extends GUIScreen
         MinecraftForge.EVENT_BUS.register(CharacterCreationGUI.class);
     }
 
+
+    protected Network.CharacterCreationGUIPacket packet;
+
+
     public CharacterCreationGUI(Network.CharacterCreationGUIPacket packet)
     {
+        this.packet = packet;
+
         Minecraft.getMinecraft().displayGuiScreen(this);
+
+
+        GUIAutocroppedView tabHolder = new GUIAutocroppedView(this);
+        tabHolder.setSubElementAutoplaceMethod(GUIElement.AP_X_0_TOP_TO_BOTTOM);
+
+        tabHolder.add(new GUITextButton(this, "Test"));
+        tabHolder.add(new GUITextButton(this, "Test2"));
+
+        root.add(tabHolder);
     }
 
 
