beda32c6bbec1ce06ec101d77afed2c78a621394
Kyle Koder <silvertallon@gmail.com>
2020-12-07 22:00:36 -0800

    1.12.2.000q (previous commit was r, but I want an explicit version commit here)

--- src/main/java/com/fantasticsource/faeruncharacters/FaerunCharacters.java
+++ src/main/java/com/fantasticsource/faeruncharacters/FaerunCharacters.java
@@ -43,7 +43,7 @@ public class FaerunCharacters
 
     public static final String MODID = "faeruncharacters";
     public static final String NAME = "Faerun Characters";
-    public static final String VERSION = "1.12.2.000q";
+    public static final String VERSION = "1.12.2.000r";
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event) throws IOException
