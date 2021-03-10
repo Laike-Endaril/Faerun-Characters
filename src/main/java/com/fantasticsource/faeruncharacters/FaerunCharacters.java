c4cc34d533724b6477f101615e4fe6999dd4ceb5
Kyle Koder <silvertallon@gmail.com>
2021-03-10 09:23:18 -0800

    1.12.2.000v

--- src/main/java/com/fantasticsource/faeruncharacters/FaerunCharacters.java
+++ src/main/java/com/fantasticsource/faeruncharacters/FaerunCharacters.java
@@ -37,7 +37,7 @@ public class FaerunCharacters
 
     public static final String MODID = "faeruncharacters";
     public static final String NAME = "Faerun Characters";
-    public static final String VERSION = "1.12.2.000u";
+    public static final String VERSION = "1.12.2.000v";
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event) throws IOException
